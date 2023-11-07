b88f90698f798ec77575ea60bdb751b3cf8ac6f3
Vlad Silagy <vladsilagy@gmail.com>
2023-11-06 21:06:00 -0500

    add player get name and symbol

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,7 @@
 class Player
+  # Use attr_reader to create getter methods for name and symbol.
   attr_reader :name, :symbol
-
+  # Initialize the player with a name and a symbol ('X' or 'O').
   def initialize(name, symbol)
     @name = name
     @symbol = symbol
